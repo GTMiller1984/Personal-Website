91a70889fe64975f689f0bbdfd94932065fb19e9
GTMiller1984 <gordon_miller@icloud.com>
2024-09-08 13:39:52 +0100

    script.js

--- script.js
+++ script.js
@@ -12,3 +12,22 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+// Modal example
+var modal = document.getElementById("myModal");
+var btn = document.getElementById("myBtn");
+var span = document.getElementsByClassName("close")[0];
+
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
